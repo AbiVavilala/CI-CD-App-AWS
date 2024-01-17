ed3fe320dd6638249200c3c2518893eab5a0dfdb
Abilabs <Abilash6891@gmail.com>
2024-01-17 19:15:31 +1100

    initial commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,7 @@
-echo "Running container..."
-docker run --name flask_app -d -p 5000:5000 642655931180.dkr.ecr.ap-southeast-2.amazonaws.com/flask_image:latest
+#!/bin/bash
+
+# Authenticate Docker with ECR
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 642655931180.dkr.ecr.ap-southeast-2.amazonaws.com
+
+# Run the Docker container
+docker run -d -p 80:80 642655931180.dkr.ecr.ap-southeast-2.amazonaws.com/flask_image:latest
