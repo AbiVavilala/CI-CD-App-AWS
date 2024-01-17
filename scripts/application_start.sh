f00ee089648bd3ccc50aa253989dbd5115d994cf
Abilabs <Abilash6891@gmail.com>
2024-01-17 19:41:26 +1100

    initial commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 642655931180.dkr.ecr.ap-southeast-2.amazonaws.com
 
 # Run the Docker container
-docker run -d -p 80:80 642655931180.dkr.ecr.ap-southeast-2.amazonaws.com/flask_image:latest
+docker run -d -p 8080:80 642655931180.dkr.ecr.ap-southeast-2.amazonaws.com/flask_image:latest
