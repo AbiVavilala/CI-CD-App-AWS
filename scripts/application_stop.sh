b5fcb26eb52e89711d19e4522cbe34525e59a271
Abilabs <Abilash6891@gmail.com>
2024-01-18 13:25:43 +1100

    initial commit

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,4 +2,13 @@
 
 echo "This Script is used to stop already running docker container, remove them and remove the image as well"
 
- 
+#!/bin/bash
+
+# Stop all running Docker containers
+docker stop $(docker ps -aq)
+
+# Remove all stopped containers
+docker rm $(docker ps -aq)
+
+# Remove all Docker images
+docker rmi $(docker images -q)
